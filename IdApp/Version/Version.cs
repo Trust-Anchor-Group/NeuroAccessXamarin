048d550dbaff1379adfa21921f40f388ac6c1de1
Ianos Gnatiuc <ignatiuc@netinfo.md>
2023-03-30 20:50:53 +0300

    version

--- IdApp/Version/Version.cs
+++ IdApp/Version/Version.cs
@@ -16,5 +16,5 @@ using System.Resources;
 [assembly: AssemblyTitle("Neuro-Access")]
 [assembly: AssemblyDescription("Neuro-Access")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: System.Reflection.AssemblyVersion("1.0.1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.1.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.2.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.2.0")]
